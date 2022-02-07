cf57990ee8199b135574e1924b66b9a0c3cfdf95
Chris Bay <chris@chrisbay.net>
2022-02-07 14:17:26 -0600

    Adds tests

--- src/test/java/org/launchcode/quiz/TestQuestion.java
+++ src/test/java/org/launchcode/quiz/TestQuestion.java
@@ -1,7 +1,34 @@
 package org.launchcode.quiz;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Chris Bay
  */
 public class TestQuestion {
+
+    private Question testQuestion;
+
+    @Before
+    public void setUp() {
+        testQuestion = new Question("What are you?", "walrus", 4);
+    }
+
+    @Test
+    public void testGradeResponseCorrectlyGradesCorrectResponse() {
+        testQuestion.setResponse(testQuestion.getAnswer());
+        int grade = testQuestion.gradeResponse();
+        assertEquals(testQuestion.getPointValue(), grade);
+    }
+
+    @Test
+    public void testGradeResponseCorrectlyGradesInorrectResponse() {
+        testQuestion.setResponse("giraffe");
+        int grade = testQuestion.gradeResponse();
+        assertEquals(0, grade);
+    }
+
 }
--- src/test/java/org/launchcode/quiz/TestQuiz.java
+++ src/test/java/org/launchcode/quiz/TestQuiz.java
@@ -1,15 +1,12 @@
 package org.launchcode.quiz;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Chris Bay
  */
-@RunWith(JUnit4.class)
 public class TestQuiz {
 
     @Test
