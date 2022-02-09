af3587ec437a829da391a54af2984b1278c8f6d2
Chris Bay <chris@chrisbay.net>
2022-02-09 12:24:06 -0600

    Adds MultipleChoiceQuestion

--- src/main/java/org/launchcode/quiz/Main.java
+++ src/main/java/org/launchcode/quiz/Main.java
@@ -8,11 +8,10 @@ public class Main {
     public static void main(String[] args) {
         Quiz quiz = new Quiz();
         quiz.addQuestion(new ShortAnswerQuestion("What is your favorite color?", "green"));
-        quiz.addQuestion(new ShortAnswerQuestion("What is the answer to the ultimate question?", "42"));
-        quiz.addQuestion(new ShortAnswerQuestion("How high is the St. Louis Arch in feet?", "630"));
-
         quiz.addQuestion(new TrueFalseQuestion("Cheese is delicious", true));
-        quiz.addQuestion(new TrueFalseQuestion("Pigs can fly", false));
+
+        String[] choices = {"Charlotte", "Asheville", "Raleigh", "Greensboro"};
+        quiz.addQuestion(new MultipleChoiceQuestion("What is the capital of North Carolina?", choices, 2));
 
         quiz.runQuiz();
     }
---
+++ src/main/java/org/launchcode/quiz/MultipleChoiceQuestion.java
@@ -0,0 +1,65 @@
+package org.launchcode.quiz;
+
+import java.util.Scanner;
+
+/**
+ * Created by Chris Bay
+ */
+public class MultipleChoiceQuestion extends Question {
+
+    private final String[] choices;
+    private int response;
+    private int answer;
+
+    // constructors
+    public MultipleChoiceQuestion(String text, String[] choices, int answer) {
+        super(text);
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    public MultipleChoiceQuestion(String text, String[] choices, int answer, int pointValue) {
+        super(text);
+        this.choices = choices;
+        this.answer = answer;
+        this.setPointValue(pointValue);
+    }
+
+    @Override
+    public void displayQuestion() {
+        System.out.println(this.getText() + " (choose one)");
+        for (int i = 0; i < choices.length; i++) {
+            System.out.println(i + " - " + choices[i]);
+        }
+    }
+
+    @Override
+    public void collectResponse(Scanner input) {
+        this.response = input.nextInt();
+    }
+
+    @Override
+    public int gradeResponse() {
+        return this.response == this.answer ? this.getPointValue() : 0;
+    }
+
+    public String[] getChoices() {
+        return choices;
+    }
+
+    public int getResponse() {
+        return response;
+    }
+
+    public void setResponse(int response) {
+        this.response = response;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+}
